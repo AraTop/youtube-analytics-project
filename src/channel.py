9309e64fc796c42a680b8a58a97e6c8bf8450468
Grigory <lololohka057@gmail.com>
2023-05-13 15:39:36 +0300

    last_

--- src/channel.py
+++ src/channel.py
@@ -14,7 +14,7 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
 
         channel = Channel.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         
@@ -26,12 +26,12 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = Channel.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         return json.dumps(channel, indent=2, ensure_ascii=False)
     
     @property
     def channel_id(self):
-        return self.channel_id
+        return self._channel_id
     
     @channel_id.setter
     def channel_id(self, name):
@@ -43,8 +43,14 @@ class Channel:
         return self.youtube
     
     def to_json(self, name_file: str):
-        json_str = f'"title":{Channel.title}, "video_count":{Channel.video_count}, "url":{Channel.url}, "view_count":{Channel.view_count}, "subscribe_count":{Channel.subscribe_count}'
-        json_obj = json.loads(json_str)
+        data = {
+            "channel_id": self.__channel_id,
+            "title": self.title,
+            "url": self.url,
+            "subscriber_count": self.subscribe_count,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+        }
  
-        with open(name_file, 'w') as f:
-            json.dump(json_obj, f)
+        with open(name_file, 'w' , encoding="utf-8") as f:
+            json.dump(data, f,ensure_ascii=False, indent=2)
